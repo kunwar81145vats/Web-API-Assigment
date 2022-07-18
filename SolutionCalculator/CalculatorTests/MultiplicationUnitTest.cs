257c1f556940294129d3926bc325dd3845231184
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-07-18 05:39:24 -0400

    Commit: Multiplication test cases Details: 10 test cases created for multiplication

--- SolutionCalculator/CalculatorTests/MultiplicationUnitTest.cs
+++ SolutionCalculator/CalculatorTests/MultiplicationUnitTest.cs
@@ -7,6 +7,114 @@ namespace CalculatorTests;
 [TestClass]
 public class MultiplicationUnitTest
 {
-    
+    [DataTestMethod]
+    [DataRow(1, 1, 1)]
+    [DataRow(2, 2, 4)]
+    [DataRow(2, 7, 14)]
+    public void MultiplyWithPositiveInts(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(1, -1, -1)]
+    [DataRow(4, -2, -8)]
+    [DataRow(1, -2, -2)]
+    public void MultiplyWithOneNegative(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(0, -1, 0)]
+    [DataRow(0, -2, 0)]
+    [DataRow(0, -5, 0)]
+    public void MultiplyWithOneNegativeAndZero(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(0, 1, 0)]
+    [DataRow(0, 2, 0)]
+    [DataRow(0, 5, 0)]
+    public void MultiplyWithZero(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(-2, -1, 2)]
+    [DataRow(-1, -5, 5)]
+    [DataRow(-4, -5, 20)]
+    public void MultiplyWithNegativeInts(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(1.2, 1.5, 1.8)]
+    [DataRow(2.1, 2.5, 5.2)]
+    [DataRow(2.1, 7.1, 14.9)]
+    public void MultiplyWithPositiveDoubles(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(1, -1.5, -1.5)]
+    [DataRow(4, -2.2, -8.8)]
+    [DataRow(1, -2.5, -2.5)]
+    public void MultiplyWithOneNegativeDouble(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(0, -1.5, 0)]
+    [DataRow(0, -2.3, 0)]
+    [DataRow(0, -5.5, 0)]
+    public void MultiplyWithOneNegativeDoubleAndZero(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(0, 1.5, 0)]
+    [DataRow(0, 2.1, 0)]
+    [DataRow(0, 5.5, 0)]
+    public void MultiplyWithZeroAndDouble(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
+
+    [DataTestMethod]
+    [DataRow(-2.1, -1.2, 2.5)]
+    [DataRow(-1.6, -5.1, 8.2)]
+    [DataRow(-4.2, -5.4, 22.7)]
+    public void MultiplyWithNegativeDouble(double left, double right, double expected)
+    {
+        double result;
+        result = Calculator.Calculate(left, right, "*");
+        Assert.AreEqual(expected, result);
+    }
 }
 
